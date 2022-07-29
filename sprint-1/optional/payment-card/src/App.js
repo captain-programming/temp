9f408f6a000ce54bae55257dd9ef319ff2c2a1d2
Dinesh Karde <dineshkarde103@gmail.com>
2022-07-29 07:46:10 +0530

    Update App.js

--- sprint-1/optional/payment-card/src/App.js
+++ sprint-1/optional/payment-card/src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <PaymentCard date="28/10/2020" heading="Case Study" subHeading="Amazon Gift Pay" device="Desktop - Mobile" logo="https://www.askgalore.com/wp-content/uploads/2020/08/amazon-icon-amazon-logo-png-icon.png" color="#FFA500"/>
 
+                  <br/><br/>
+                  
       <PaymentCard date="17 Sep 2020" heading="Case Study" subHeading="Apple Gift Payment" device="MacOS - Mobile" logo="https://logolook.net/wp-content/uploads/2021/06/Apple-Logo.svg" color="#F5F5F5"/>
     </div>
   );
